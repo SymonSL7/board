a1d35a25bf595576d7f7b5b22b0180c021d67e98
Symon Silva Lima <symonlima07@gmail.com>
2025-02-25 18:53:26 -0300

    Board
    Board de tarefas, com banco de dados Mysql (Local).

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202502251719.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset Symon:202502251719
+--comment: set unblock_reason nullable
+
+ALTER TABLE BLOCKS MODIFY COLUMN unblock_reason VARCHAR(255) NULL;
+
+--rollback ALTER TABLE BLOCKS MODIFY COLUMN unblock_reason VARCHAR(255) NOT NULL;
